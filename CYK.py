04f904238a89ba80904ea7474bba2f26073eeaec
bangkitdc <bangkitdc@gmail.com>
2022-11-22 01:41:52 +0700

    feat: add fa and cyk algorithm

--- CYK.py
+++ CYK.py
@@ -1,24 +1,69 @@
+# contoh
+# NP   ->  Det | Nom
+# Nom  ->  AP | Nom
+# AP   ->  Adv | A
+# Det  ->  a | an
+# Adv  ->  very | extremely
+# AP   ->  heavy | orange | tall
+# A    ->  heavy | orange | tall | muscular
+# Nom  ->  book | orange | man
+
+# Non-terminal symbols
+non_terminals = ["NP", "Nom", "Det", "AP",
+                  "Adv", "A"]
+
+# Terminal symbols
+terminals = ["book", "orange", "man",
+             "tall", "heavy",
+             "very", "muscular"]
+
+# Rules of the grammar
+R = {
+     "NP": [["Det", "Nom"]],
+     "Nom": [["AP", "Nom"], ["book"],
+             ["orange"], ["man"]],
+     "AP": [["Adv", "A"], ["heavy"],
+            ["orange"], ["tall"]],
+     "Det": [["a"]],
+     "Adv": [["very"], ["extremely"]],
+     "A": [["heavy"], ["orange"], ["tall"],
+           ["muscular"]]
+    }
+
+# w = "a very heavy orange book".split()
+# n = len(w)
+
+# T = [[set([]) for i in range(n)] for j in range(n)]
+
+# for i in range(n):
+#     for LHS, RHS in R.items():  # prodRule = LHS : RHS (1 | 2 | .. | N)
+#         for elements in RHS:
+#             print(elements)
+
 def cyk(w, cnfGrammar):
     n =  len(w)
-    dp = [[set([]) for i in range(n)] for j in range(n)]
 
-    for i in range(n):
-        for var in cnfGrammar.items():
-            for terminal in var[1]:
-                if len(terminal) == 1 and terminal[0] == w[i]:
-                    dp[i][i].add(var[0])
+    # CYK Table
+    T = [[set([]) for j in range(n)] for i in range(n)]
+
+    for j in range(n):
+        for LHS, RHS in cnfGrammar.items():  # prodRule = LHS : RHS (1 | 2 | .. | N)
+            for elements in RHS:             # elements = [A, B, C]
+                # if terminal is found
+                if len(elements) == 1 and elements[0] == w[j]:
+                    T[j][j].add(LHS)
     
-    for l in range(2, n + 1):
-        for i in range(0, n - l + 1):
-            j = i + l - 1
-            for k in range(i, j):
-                for var in cnfGrammar.items():
-                    for production in var[1]:
-                        if len(prod) == 2:
-                            if (prod[0] in dp[i][k]) and (prod[1] in dp[k + 1][j]):
-                                dp[i][j].add(var[0])
-    
-    if "S0" in dp[0][n - 1]:
+        for i in range(j, -1, -1):
+            # iterate over i - (j + 1)
+            for k in range(i, j + 1):
+                # iterate over the rules
+                for LHS, RHS in cnfGrammar.items():
+                    for elements in RHS:
+                        # if 2 variables is found
+                        if len(elements) == 2 and elements[0] in T[i][k] and elements[1] in T[k + 1][j]:
+                            T[i][j].add(LHS)
+
+    if "S0" in T[0][n - 1]:
         print("Accepted Answer!")
     else:
         print("Syntax Error!")
---
+++ FA.py
@@ -0,0 +1,73 @@
+def q1(c):  # input character pertama (start state)
+    if (65 <= ord(c) <= 90):        # A - Z
+        state = 2
+    elif (ord(c) == 95):            # _
+        state = 2
+    elif (97 <= ord(c) <= 122):     # a - z
+        state = 2
+    else:
+        state = 3
+    return state
+
+def q2(c):  # final state
+    if (65 <= ord(c) <= 90):        # A - Z
+        state = 2
+    elif (ord(c) == 95):            # _
+        state = 2
+    elif (97 <= ord(c) <= 122):     # a - z
+        state = 2
+    elif (48 <= ord(c) <= 57)       # 0 - 9
+        state = 2
+    else:
+        state = 3
+    return state
+
+def q3(c):  # input character yang salah (dead state)
+    state = 3
+    return state
+
+def isValidVariable(s):
+    state = 1
+    for i in range(len(s)):
+        if (state == 1):
+            state = q1(s[i])
+        if (state == 2):
+            state = q2(s[i])
+        if (state == 3):
+            state = q3(s[i])
+    if (state == 2): # final state
+        return True
+    else:
+        return False
+
+def q4(c):  # input character pertama (start state)
+    if (48 <= ord(c) <= 57):        # 0 - 9
+        state = 5
+    else:
+        state = 6
+    return state
+
+def q5(c):  # final state
+    if (48 <= ord(c) <= 57):
+        state = 5
+    else:
+        state = 6
+    return state
+
+def q6(c): # input character yang salah (dead state)
+    state = 6
+    return state
+
+def isValidNumber(s):
+    state = 4
+    for i in range(llen(s)):
+        if (state == 4):
+            state = q4(s[i])
+        if (state == 5):
+            state = q5(s[i])
+        if (state == 6):
+            state = q6(s[i])
+    if (state == 5): # final state
+        return True
+    else:
+        return False
