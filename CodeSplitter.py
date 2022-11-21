25867923f48ffd75878aefb26652c2c0d72c3561
bangkitdc <bangkitdc@gmail.com>
2022-11-22 01:45:23 +0700

    add codesplitter(not done)

--- CodeSplitter.py
+++ CodeSplitter.py
@@ -1,3 +1,17 @@
 import re       #regular expression
 import FA as fa
 
+def splitCode(filename):
+    f = open(filename, "r")
+    inputFile = f.read()
+    f.close()
+
+    result = []
+
+    # ignore blanks
+    inputFile = inputFile.split(" ")
+    for statement in inputFile:
+        if statement != '':
+            result.append(statement)    # insert to result
+    
+    
