baebd7a81bbfa5e91efaa21e71cde0d19f41ada2
bagas003 <bagasaryo03@gmail.com>
2022-11-24 13:33:52 +0700

    CFGtoCNF

--- CFGtoCNF.py
+++ CFGtoCNF.py
@@ -1,13 +1,13 @@
 import string
 
-def readGrammar(filepath):
+def readCFG(filepath):
     file = open(filepath, 'r').read()
     terminals = file.split("Variables:")[0]
     variables = file.split("Variables:")[1].split("Productions:")[0]
     prods = file.split("Productions:\n")[1]
 
-    terminals = terminals.replace("Terminals","").replace("\n","").split(' ')
-    variables = variables.replace("variables","").replace("\n","").split(' ')
+    terminals = terminals.replace("Terminals:","").replace("\n","").split(' ')
+    variables = variables.replace("variables:","").replace("\n","").split(' ')
     prods = prods.replace("Production:","").split('\n')
 
     newprods = []
@@ -20,4 +20,70 @@ def readGrammar(filepath):
     return terminals, variables, newprods
 
 
-print(readGrammar('grammarfixbrow.txt'))
+def generateVar(V):
+    if V[1] == '9':
+        return chr(ord(V[0])+1) + '1'
+    return V[0] + chr(ord(V[1])+1)
+
+
+def simplify(variables, productions):
+    newprods = []
+    newvar = 'A0'
+    for prod in productions:
+        k = len(prod[1])
+        if k <= 2:
+            newprods.append(prod)
+        else:
+            newvar = generateVar(newvar)
+            variables.append(newvar+'1')
+            newprods.append((prod[0], [prod[1][0]] + [newvar+'1']))
+
+            for i in range(1,k-2):
+                var1 = newvar+str(i)
+                var2 = newvar+str(i+1)
+                variables.append(var2)
+                newprods.append((var1, [prod[1][i], var2]))
+            
+            newprods.append((newvar+str(k-2), prod[1][k-2:k]))
+    return newprods
+
+
+def isUnitary(prod, variables):
+    return len(prod[1]) == 1 and prod[0] in variables and prod[1][0] in variables
+
+
+def isUnitaryExist(productions, variables):
+    for prod in productions:
+        if isUnitary(prod, variables): return True
+    return False
+
+
+def eliminateUnitary(productions, variables):
+    newprods = []
+    for prod in productions:
+        if isUnitary(prod, variables):
+            for iter_prod in productions:
+                if prod[1][0] == iter_prod[0] and prod[0] != iter_prod[0]:
+                    newprods.append((prod[0], iter_prod[1]))
+        else:
+            newprods.append(prod)
+    return newprods
+
+
+def makeCNFdictionary(productions):
+    dictionary = {}
+    for prod in productions:
+        if prod[0] in dictionary.keys():
+            dictionary[prod[0]].append(prod[1]) 
+        else:
+            dictionary[prod[0]] = []
+            dictionary[prod[0]].append(prod[1])
+    return dictionary
+
+
+def CFGtoCNF(filepath):
+    terminals, variables, productions = readCFG(filepath)
+    productions = simplify(productions, variables)
+    while isUnitaryExist(productions, variables):
+        productions = eliminateUnitary(productions, variables)
+    return makeCNFdictionary(productions)
