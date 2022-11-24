68d6bec0db81925e601764d366705c5c8ffd7a8b
zaydanA <16521131@std.stei.itb.ac.id>
2022-11-24 23:19:08 +0700

    .

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import ProgressBar as PB
 import splashscreen
 
 
-splashscreen.splash()
+
 t1 = threading.Thread(target=PB.ProgressBar, name='t1')
+splashscreen.splash()
 t1.start()
 
 CNFGrammar = CFGtoCNF.CFGtoCNF("grammarfixfix.txt")
 filename = sys.argv[1]
 print("Please wait! Compiling ...")
 output = split.splitCode(filename)
-
+flag = CYK.cyk(output, CNFGrammar)
 
 t1.join()
-flag = CYK.cyk(output, CNFGrammar)
 if flag :
     print("Accepted Answer!")
 else:
     print("Syntax Error!")
+
