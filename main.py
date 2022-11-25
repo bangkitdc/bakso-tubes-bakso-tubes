3565c3a902866fceeec19a40e4ca56cfb7337a40
bagas003 <bagasaryo03@gmail.com>
2022-11-25 18:52:21 +0700

    validasy sys.argv

--- main.py
+++ main.py
@@ -11,24 +11,30 @@ t1 = threading.Thread(target=src.splashscreen.ProgressBar, name='t1')
 
 # Splash screen
 src.splashscreen.splash()
-t1.start()
+
 
 # CFG -> CNF (CFG from .txt)
 CNFGrammar = src.CFGtoCNF.CFGtoCNF("txt/grammar.txt")
 filename = "test/" +  sys.argv[1]
-output = split.splitCode(filename)
+
 print("Please wait! Compiling ...")
 
-# Read testfile (.js)
-output = split.splitCode(filename)
+try:
+    
+    # Read testfile (.js)
+    output = split.splitCode(filename)
+    t1.start()
 
-# Check with CYK algorithm
-flag = src.CYK.cyk(output, CNFGrammar)
+    # Check with CYK algorithm
+    flag = src.CYK.cyk(output, CNFGrammar)
 
-t1.join()
-if flag :
-    src.splashscreen.splashAcc()
-    print("Accepted "+colorama.Fore.WHITE+"Answer!\n")
-else:
-    src.splashscreen.splashError()
-    print(colorama.Fore.WHITE+"Syntax"+colorama.Fore.RED+" Error!\n")
+    t1.join()
+    if flag :
+        src.splashscreen.splashAcc()
+        print("Accepted "+colorama.Fore.WHITE+"Answer!\n")
+    else:
+        src.splashscreen.splashError()
+        print(colorama.Fore.WHITE+"Syntax"+colorama.Fore.RED+" Error!\n")
+
+except:
+    print("filenya gaada syg")
